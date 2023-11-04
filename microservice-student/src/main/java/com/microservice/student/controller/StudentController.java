77fba4a7aa96652fb83aded14ae34e8daf157cdf
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-03 23:20:17 -0300

    Criando o controller StudentController

---
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.microservice.student.controller;
+
+import com.microservice.student.entities.Student;
+import com.microservice.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveStudent(@RequestBody Student student) {
+        studentService.save(student);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllStudent() {
+        return ResponseEntity.ok(studentService.findall());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(Long id) {
+        return ResponseEntity.ok(studentService.findById(id));
+    }
+
+}
