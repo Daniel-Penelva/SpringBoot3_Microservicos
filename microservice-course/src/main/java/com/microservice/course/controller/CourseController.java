9859103837db9c0cc1aa112da6a2b5321e000b47
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-06 23:13:08 -0300

    Criando os imports sql para inserir os dados nas tabelas courses e students + algumas alterações

--- microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
+++ microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
@@ -14,22 +14,26 @@ public class CourseController {
     @Autowired
     private ICourseService courseService;
 
+    // Criar um curso - http://localhost:9090/api/course/create
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveCourse(@RequestBody Course course) {
         courseService.save(course);
     }
 
+    // Buscar todos os cursos - http://localhost:9090/api/course/all
     @GetMapping("/all")
     public ResponseEntity<?> findAllCourse() {
         return ResponseEntity.ok(courseService.findAll());
     }
 
+    // Buscar um curso por id - http://localhost:9090/api/course/search/2
     @GetMapping("/search/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return ResponseEntity.ok(courseService.findById(id));
     }
 
+   // Buscar estudantes em relação ao id de um curso - http://localhost:9090/api/course/search-student/2
     @GetMapping("search-student/{idCourse}")
     public ResponseEntity<?> findStudentsByIdCourse(@PathVariable("idCourse") Long idCourse){
         return ResponseEntity.ok(courseService.findStudentsByIdCourse(idCourse));
--- microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -14,22 +14,26 @@ public class StudentController {
     @Autowired
     private IStudentService studentService;
 
+    // Criar um estudante - http://localhost:8090/api/student/create
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveStudent(@RequestBody Student student) {
         studentService.save(student);
     }
 
+    // Buscar todos os estudantes - http://localhost:8090/api/student/all
     @GetMapping("/all")
     public ResponseEntity<?> findAllStudent() {
         return ResponseEntity.ok(studentService.findall());
     }
 
+    // Buscar estudante por id - http://localhost:8090/api/student/search/1
     @GetMapping("/search/{id}")
-    public ResponseEntity<?> findById(Long id) {
+    public ResponseEntity<?> findById(@PathVariable Long id) {
         return ResponseEntity.ok(studentService.findById(id));
     }
 
+    // Buscar estudantes por id do curso - http://localhost:8090/api/student/search-ny-course/2
     @GetMapping("/search-ny-course/{idCourse}")
     public ResponseEntity<?> findByIdCourse(@PathVariable("idCourse") Long idCourse){
         return ResponseEntity.ok(studentService.findByIdCourse(idCourse));
